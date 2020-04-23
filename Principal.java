ac7883987afa9d10feae8f591b7536bea050cbad
Adilem Dobras <59998402+adobrasca512@users.noreply.github.com>
2020-04-23 16:27:37 +0200

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,25 @@
+package ProyectoIngenieria;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Viniciarsesion window1= new Viniciarsesion();
+		window1.setVisible(true);
+	
+		DatosCliente datos=new DatosCliente("", "", "", "","", 0, 0, "");
+	  // Conf_Iniciosesion configura=new Conf_Iniciosesion();
+	   //configura.Ficheronuevo();
+		//int desicion=0;
+		//shop.Menu_Clientes(desicion);
+		//Micuenta cuenta=new Micuenta();
+		//
+		//DatosCliente cliente = new DatosCliente("", "", "", "", "", 0, 0, "");
+		//Administradortabla prueba = new Administradortabla();
+	   //	String ruta= "tableconvert_csv_0xo8do.csv";
+        //String matriz[][]=prueba.getLectura_especial_clientes(ruta);
+        //prueba.getPintarMatriz(cliente.incluirClientes(matriz, "", "", "", "", "", 0, 0, ""));
+        
+		//prueba.getescribirenFichero(ruta, cliente.incluirClientes(matriz, "", "", "", "", "", 0, 0, ""));
+	}
+
+}
